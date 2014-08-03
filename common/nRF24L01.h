7be3fb46fe4fee8f528d264e63f9c134ddd0069e
Pradeepa Senanayake <pradeepa.kck@gmail.com>
2014-08-03 09:11:37 +0530

    Added BITx definitions

--- common/nRF24L01.h
+++ common/nRF24L01.h
@@ -30,6 +30,17 @@
 #define _NRF24L01_H
 
 
+#define	BIT0	1 << 0
+#define	BIT1	1 << 1
+#define	BIT2	1 << 2
+#define	BIT3	1 << 3
+#define	BIT4	1 << 4
+#define	BIT5	1 << 5
+#define	BIT6	1 << 6
+#define	BIT7	1 << 7
+
+
+
 
 /* Register Map */
 #define RF24_CONFIG      0x00
