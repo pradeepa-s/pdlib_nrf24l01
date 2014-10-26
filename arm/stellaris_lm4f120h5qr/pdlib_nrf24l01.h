bc0b06636ce20bc6341a9329a05272f4f4a48afc
Pradeepa Senanayake <pradeepa.kck@gmail.com>
2014-10-26 15:31:58 +0530

    The major changes are as follows, [1]. Made the register write functions to non-static and moved them to the header file to make sure those can be accessed from outside the '.c' file.
    [2]. Changed the Init function to add new parameters to the function to
    receive peripheral-bit-masks for CE and CSN functions.
    
    [3]. Changed the register init function to init all the registers.
    
    [4]. Changed the PA gain function to make the parameter values more user
    friendly.
    
    [5]. Changed the enable tx mode function to clear interrupt flags prior to
    putting the module in the TX mode
    
    [6]. Added functions to determine the state of the TX fifo.
    
    [7]. Added a function to wait until the TX is over
    
    [8]. Added a function to read multibyte registers
    
    [9]

--- arm/stellaris_lm4f120h5qr/pdlib_nrf24l01.h
+++ arm/stellaris_lm4f120h5qr/pdlib_nrf24l01.h
@@ -3,29 +3,38 @@
 
 /* PS: Function prototypes */
 
-void NRF24L01_Init(unsigned long ulCEBase, unsigned long ulCEPin, unsigned long ulCSNBase, unsigned long ulCSNPin,unsigned char ucSSIIndex);
+void NRF24L01_Init(unsigned long ulCEBase, unsigned long ulCEPin, unsigned long ulCEPeriph, unsigned long ulCSNBase, unsigned long ulCSNPin, unsigned long ulCSNPeriph, unsigned char ucSSIIndex);
 void NRF24L01_RegisterInit();
+
+/* Configuration related */
 void NRF24L01_PowerDown();
 void NRF24L01_PowerUp();
 void NRF24L01_FlushTX();
 void NRF24L01_FlushRX();
 void NRF24L01_SetAirDataRate(unsigned char ucDataRate);
 void NRF24L01_SetLNAGain(unsigned char ucLNAGain);
-void NRF24L01_SetPAGain(unsigned char ucPAGain);
+void NRF24L01_SetPAGain(int iPAGain);
 void NRF24L01_SetRFChannel(unsigned char ucRFChannel);
 unsigned char NRF24L01_GetStatus();
 
+/* TX mode related */
 void NRF24L01_SetTXAddress(unsigned char* pucAddress);
-void NRF24L01_SetTxPayload(unsigned char* pucData, unsigned int uiLength);
+int NRF24L01_SetTxPayload(unsigned char* pucData, unsigned int uiLength);
 void NRF24L01_EnableTxMode();
-//unsigned char NRF24L01_IsTxComplete();
+int NRF24L01_IsTxFifoFull();
+int NRF24L01_WaitForTxComplete(unsigned char ucWaitMode);
 
+/* RX mode related */
 void NRF24L01_SetRxAddress(unsigned char ucDataPipe, unsigned char *pucAddress);
 void NRF24L01_EnableRxMode();
-unsigned char NRF24L01_IsDataReadyRx();
+int NRF24L01_IsDataReadyRx();
 unsigned char NRF24L01_GetRxDataAmount(unsigned char ucDataPipe);
 
+/* Register level access */
 unsigned char NRF24L01_RegisterRead_8(unsigned char ucRegister);
+unsigned char NRF24L01_RegisterRead_Multi(unsigned char ucRegister, unsigned char *pucBuffer, unsigned int uiLength);
+void NRF24L01_RegisterWrite_8(unsigned char ucRegister, unsigned char ucValue);
+void NRF24L01_RegisterWrite_Multi(unsigned char ucRegister, unsigned char *pucData, unsigned int uiLength);
 
 #endif
 
