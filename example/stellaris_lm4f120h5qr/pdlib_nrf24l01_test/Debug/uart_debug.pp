6a27007d775369405164ebd69aa0943a169f7f6c
Pradeepa Senanayake <pradeepa.kck@gmail.com>
2015-03-29 20:50:23 +0530

    Revert "Removed the example files"
    This reverts commit 22680deb7c7a3b272a97aba4bfa200a692be1ca1.

---
+++ example/stellaris_lm4f120h5qr/pdlib_nrf24l01_test/Debug/uart_debug.pp
@@ -0,0 +1,35 @@
+# FIXED
+
+uart_debug.obj: ../uart_debug.c
+uart_debug.obj: ../uart_debug.h
+uart_debug.obj: F:/ti/ccsv5/tools/compiler/arm_5.0.1/include/stdio.h
+uart_debug.obj: F:/ti/ccsv5/tools/compiler/arm_5.0.1/include/linkage.h
+uart_debug.obj: F:/ti/ccsv5/tools/compiler/arm_5.0.1/include/stdarg.h
+uart_debug.obj: F:/StellarisWare/inc/hw_memmap.h
+uart_debug.obj: F:/StellarisWare/inc/hw_types.h
+uart_debug.obj: F:/StellarisWare/inc/hw_uart.h
+uart_debug.obj: F:/StellarisWare/inc/hw_sysctl.h
+uart_debug.obj: F:/StellarisWare/inc/hw_gpio.h
+uart_debug.obj: F:/StellarisWare/driverlib/gpio.h
+uart_debug.obj: F:/StellarisWare/driverlib/pin_map.h
+uart_debug.obj: F:/StellarisWare/driverlib/ssi.h
+uart_debug.obj: F:/StellarisWare/driverlib/sysctl.h
+uart_debug.obj: F:/StellarisWare/driverlib/rom.h
+uart_debug.obj: F:/StellarisWare/driverlib/uart.h
+
+../uart_debug.c: 
+../uart_debug.h: 
+F:/ti/ccsv5/tools/compiler/arm_5.0.1/include/stdio.h: 
+F:/ti/ccsv5/tools/compiler/arm_5.0.1/include/linkage.h: 
+F:/ti/ccsv5/tools/compiler/arm_5.0.1/include/stdarg.h: 
+F:/StellarisWare/inc/hw_memmap.h: 
+F:/StellarisWare/inc/hw_types.h: 
+F:/StellarisWare/inc/hw_uart.h: 
+F:/StellarisWare/inc/hw_sysctl.h: 
+F:/StellarisWare/inc/hw_gpio.h: 
+F:/StellarisWare/driverlib/gpio.h: 
+F:/StellarisWare/driverlib/pin_map.h: 
+F:/StellarisWare/driverlib/ssi.h: 
+F:/StellarisWare/driverlib/sysctl.h: 
+F:/StellarisWare/driverlib/rom.h: 
+F:/StellarisWare/driverlib/uart.h: 
